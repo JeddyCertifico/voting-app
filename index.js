759148731809fa328e38f4024844df9553d772d4
Jeddy Certifico <jeddycolon.certifico@bicol-u.edu.ph>
2023-05-09 10:45:18 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+
+const http = require("http");
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const PORT = process.env.PORT || 8000;
+
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/result", function (req, res) {
+  res.render("pages/index");
+});
+
+app.get("/vote", function (req, res) {
+  res.render("pages/vote");
+});
+
+const languages = ["javascript", "php", "python", "java"];
+
+io.sockets.on("connection", (socket) => {
+  for (let i = 0; i < languages.length; i++) {
+    const language = languages[i];
+    socket.on(`vote_${language}`, (vote) => {
+      io.sockets.emit(`vote_${language}`, vote);
+    });
+  }
+});
+
+app.use("/views", express.static("views"));
+app.use("/public", express.static("public"));
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
